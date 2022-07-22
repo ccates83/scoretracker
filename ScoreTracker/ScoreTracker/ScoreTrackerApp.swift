58339f39500a71150ce29326826a1fb027358668
Connor <cates.connor@gmail.com>
2022-07-22 18:29:49 -0400

    initial commit

---
+++ ScoreTracker/ScoreTracker/ScoreTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreTrackerApp.swift
+//  ScoreTracker
+//
+//  Created by Connor Cates on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ScoreTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
